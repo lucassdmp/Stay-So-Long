ddf0baecc2a84395c18d92c10ef1aca18c428079
Lucas <lucassdmp01@gmail.com>
2023-04-24 16:48:43 -0300

    basic files && CMAKE setup

---
+++ build/CMakeFiles/Stay-So-Long.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stay-So-Long.dir/src/main.cpp.o"
+  "CMakeFiles/Stay-So-Long.dir/src/main.cpp.o.d"
+  "Stay-So-Long"
+  "Stay-So-Long.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Stay-So-Long.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <SFML/Window.hpp>
+#include <iostream>
+
+int main(){
+    std::cout << "TESTE" << "\nTESTE 2";
+
+    return 0;
+}
