fd8b95532bd1195b28ca7a870afe5f4b6935c2a7
Lucas <lucassdmp01@gmail.com>
2023-04-26 09:07:33 -0300

    Removed build from src, added basic matrix configuration

--- build/CMakeFiles/Stay-So-Long.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Stay-So-Long.
-# This may be replaced when dependencies are built.
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
-#include <iostream>
+#include <glm/glm.hpp>
 
 #define SCREEN_WIDTH 1000
 #define SCREEN_HEIGHT 720
@@ -8,10 +8,13 @@
 int main(){
     sf::Window window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Stay So Long", sf::Style::Fullscreen);
 
-    while(window.isOpen()){
-        //glBegin();
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, window.getSize().x, window.getSize().y, 0, 0, 1);
+    glMatrixMode(GL_MODELVIEW);
 
 
+    while(window.isOpen()){    
         sf::Event event;
         while(window.pollEvent(event)){
             if(event.type == sf::Event::Closed)
@@ -20,6 +23,17 @@ int main(){
             if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
                 window.close();
         }
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_QUADS);
+        glColor3f(1.0f, 1.0f, 1.0f);
+        glVertex2i(0, 0);
+        glVertex2i(100, 0);
+        glVertex2i(100, 100);
+        glVertex2i(0, 100);
+        glEnd();
+
+        window.display();
     }
 
     return 0;
