cfe30a1ddd4e469fb129a5a4da9e10b66b8de254
Mateus Sousa <mateussdamiano@gmail.com>
2023-05-28 16:35:19 -0300

    removed wave function from functions.hpp

--- src/Util/functions.cpp
+++ src/Util/functions.cpp
@@ -4,13 +4,6 @@
 #include <GL/gl.h>
 #include <random>
 
-float wave(float time, float from, float to, float duration) {
-    float amplitude = (to - from) / 2.0f;
-    float frequency = 1.0f / duration;
-    float phase = from + amplitude;
-    return amplitude * sin(2 * M_PI * frequency * time) + phase;
-}
-
 bool checkCollision(GameObject &obj1, GameObject &obj2)
 {
     float distance_x = obj1.getPos().x - obj2.getPos().x;
--- src/Util/functions.hpp
+++ src/Util/functions.hpp
@@ -3,8 +3,6 @@
 #include "GameObject.hpp"
 #include <glm/vec2.hpp>
 
-float wave(float time, float from, float to, float duration);
-
 bool checkCollision(GameObject &obj1, GameObject &obj2);
 
 bool isOutOfBounds(GameObject &obj, sf::RenderWindow &window);
